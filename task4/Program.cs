6e548ecb308af606e5ad0ebdf783e73dfb0ed379
akifnihat <akifst@code.edu.az>
2024-02-19 16:43:16 +0400

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+//Console.WriteLine("Hello, World!");
+#region 5
+//int[] arr = { 2, 5, 9, 11, 15, 17 };
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum += arr[i];
+//}
+//Console.WriteLine(sum);
+#endregion
+
+#region 7
+//int[] arr = { 1, 4, 29, 34, 7, 8, 9, 10 };
+//int ilk = arr[0];
+//int son = arr[arr.Length - 1];
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//            sum = ilk + son;
+//}
+//Console.WriteLine(sum); 
+#endregion
